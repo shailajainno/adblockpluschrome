f191a51610b79b437ea20b746463e30100be1419
techu <shailaja.s@innovify.in>
2018-05-14 16:48:11 +0530

    Bugs Resolved

--- devenv.gecko/replace.js
+++ devenv.gecko/replace.js
@@ -1,40 +1,37 @@
 $(function () {
     var replaceGener8;
-    setTimeout(function () {
-        let currentTimeout;
-        replaceGener8 = () => {
-            if (currentTimeout) clearTimeout(currentTimeout);
-            currentTimeout = setTimeout(function () {
-                var getAdTags = $('.gener8');
-                var arrayIframes = [];
-                var ArrayNodes = Array.prototype.slice.call(getAdTags);
-                console.log("getAdTags", getAdTags);
-                ArrayNodes.forEach(function (node, index) {
-                    var iframe = $(node).find('iframe');
-                    if (iframe.length !== 0) {
-                        var iframeGenere = document.createElement("iframe");
-                        iframeGenere.height = $(iframe)[0].height;
-                        iframeGenere.width = $(iframe)[0].width;
-                        iframeGenere.src = "https://res.cloudinary.com/djpktt9hp/image/upload/v1525686806/gen.png";
-                        iframeGenere.setAttribute('class', 'gener8Ad');
-                        iframeGenere.scrolling = "no";
-                        $(iframeGenere).css('border', '1px solid red');
-                        arrayIframes.push(iframeGenere);
-                        $(node).children().remove();
+    let currentTimeout;
+    replaceGener8 = () => {
+        if (currentTimeout) clearTimeout(currentTimeout);
+        currentTimeout = setTimeout(function () {
+            var getAdTags = $('.gener8');
+            var arrayIframes = [];
+            var ArrayNodes = Array.prototype.slice.call(getAdTags);
+            ArrayNodes.forEach(function (node, index) {
+                var iframe = $(node).find('iframe');
+                if (iframe.length !== 0) {
+                    var iframeGenere = document.createElement("iframe");
+                    iframeGenere.height = $(iframe)[0].height;
+                    iframeGenere.width = $(iframe)[0].width;
+                    iframeGenere.src = "https://res.cloudinary.com/djpktt9hp/image/upload/v1525686806/gen.png";
+                    iframeGenere.setAttribute('class', 'gener8Ad');
+                    iframeGenere.scrolling = "no";
+                    $(iframeGenere).css('border', '1px solid red');
+                    arrayIframes.push(iframeGenere);
+                    $(node).children().remove();
 
-                        var divGener8 = document.createElement("div");
-                        divGener8.setAttribute('class', 'gener8Container');
-                        divGener8.style = dumpCSSText(node);
-                        $(divGener8).css('display', '');
-                        $(divGener8).append(iframeGenere);
-                        $(node).after(divGener8);
-                        $(node).remove();
-                    }
-                });
-            }, 2000);
-        }
-        replaceGener8();
-    }, 2000);
+                    var divGener8 = document.createElement("div");
+                    divGener8.setAttribute('class', 'gener8Container');
+                    divGener8.style = dumpCSSText(node);
+                    $(divGener8).css('display', '');
+                    $(divGener8).append(iframeGenere);
+                    $(node).after(divGener8);
+                    $(node).remove();
+                }
+            });
+        }, 2000);
+    }
+    replaceGener8();
 
     function dumpCSSText(element) {
         var s = '';
